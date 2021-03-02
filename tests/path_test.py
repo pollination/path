1448685f69bc12a46a61cc25c6bf725f32cf545e
Chris Mackey <chris@mackeyarchitecture.com>
2021-03-01 22:27:46 -0500

    fix(copy): Add a function for copying to multiple destinations

--- tests/path_test.py
+++
@@ -1,8 +0,0 @@
-from pollination.path.copy import Copy
-from queenbee.plugin.function import Function
-
-
-def test_copy():
-    function = Copy().queenbee
-    assert function.name == 'copy'
-    assert isinstance(function, Function)
